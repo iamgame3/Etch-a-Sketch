d116bb5017314a0810696e8edba3b6df086bc828
iamgame3 <kosmosworld9@hotmail.com>
2022-11-27 16:48:16 -0500

    Add function to make custom grid size and add button event listener

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 const grid = document.querySelector('.grid');
 const square = document.createElement('div');
 square.classList.add('square');
+const button = document.querySelector('.change');
 
 let makeGrid = (a) => {
-    if (a > 100) return;
+    grid.replaceChildren();
     let totalSquares = a * a;
     let length = (800/a) - 2;
     for (i = 0; i < totalSquares; i++) {
@@ -13,9 +14,24 @@ let makeGrid = (a) => {
     }
 }
 
-makeGrid(8);
+makeGrid(16);
 
 let squares = document.querySelectorAll('.square')
 squares.forEach(square => square.addEventListener('mouseover', () => {
     square.style.cssText += "background-color: black;";
 }));
+
+let makeGridFromInput = () => {
+    let input = prompt("Enter preferred width (width and height are equal, and max width is 100).")
+    if (input > 100) {
+        alert("Error, max width is 100.");
+        return;
+    }
+    makeGrid(input);
+    let squares = document.querySelectorAll('.square')
+    squares.forEach(square => square.addEventListener('mouseover', () => {
+        square.style.cssText += "background-color: black;";
+    }));
+};
+
+button.addEventListener('click', makeGridFromInput);
