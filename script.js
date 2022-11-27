d4eb5f85772ab21a75ff97569016909493984597
iamgame3 <kosmosworld9@hotmail.com>
2022-11-26 19:49:04 -0500

    Add div to center grid div, and add basic pesdoucode for grid

---
+++ script.js
@@ -0,0 +1,3 @@
+// make a function that creates a 16x16 (will be changed later) grid of squares
+//     16 divs of equal width (900px/16) and of height equal to width
+//     15 divs of the same size under those divs
