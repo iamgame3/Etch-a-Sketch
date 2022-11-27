6453d130f575f90aa160147f9636f92e243ce5f5
iamgame3 <kosmosworld9@hotmail.com>
2022-11-27 18:13:40 -0500

    Add shading styles, shading function, and change eventlisteners for shading

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+let addShade = (a) => {
+    if (a.classList.contains('shade10')) {
+        a.classList.remove('shade10');
+        a.classList.add('shade20');
+    }   else if (a.classList.contains('shade20')) {
+        a.classList.remove('shade20');
+        a.classList.add('shade30');
+    }   else if (a.classList.contains('shade30')) {
+        a.classList.remove('shade30');
+        a.classList.add('shade40');
+    }   else if (a.classList.contains('shade40')) {
+        a.classList.remove('shade40');
+        a.classList.add('shade50');
+    }   else if (a.classList.contains('shade50')) {
+        a.classList.remove('shade50');
+        a.classList.add('shade60');
+    }   else if (a.classList.contains('shade60')) {
+        a.classList.remove('shade60');
+        a.classList.add('shade70');
+    }   else if (a.classList.contains('shade70')) {
+        a.classList.remove('shade70');
+        a.classList.add('shade80');
+    }   else if (a.classList.contains('shade80')) {
+        a.classList.remove('shade80');
+        a.classList.add('shade90');
+    }   else if (a.classList.contains('shade90')) {
+        a.classList.remove('shade90');
+        a.classList.add('shade100');
+    }   else if (a.classList.contains('shade100')) {
+        return;
+    }   else a.classList.add('shade10');
+};
 
 const grid = document.querySelector('.grid');
 const square = document.createElement('div');
@@ -18,7 +50,7 @@ makeGrid(16);
 
 let squares = document.querySelectorAll('.square')
 squares.forEach(square => square.addEventListener('mouseover', () => {
-    square.style.cssText += "background-color: black;";
+    addShade(square);
 }));
 
 let makeGridFromInput = () => {
@@ -30,7 +62,7 @@ let makeGridFromInput = () => {
     makeGrid(input);
     let squares = document.querySelectorAll('.square')
     squares.forEach(square => square.addEventListener('mouseover', () => {
-        square.style.cssText += "background-color: black;";
+        addShade(square);
     }));
 };
 
