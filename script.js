ba4d43aa04a121d45e8da2ede6c742f64188c547
iamgame3 <kosmosworld9@hotmail.com>
2022-11-26 20:38:28 -0500

    Edit square and grid style, and finish grid function

--- script.js
+++ script.js
@@ -3,10 +3,16 @@
 //     15 divs of the same size under those divs
 const grid = document.querySelector('.grid');
 const square = document.createElement('div');
+square.classList.add('square');
 
 let makeGrid = (a) => {
     if (a > 100) return;
     let totalSquares = a * a;
-    for (i = 0; i <= totalSquares; i++)
-        grid.appendChild(square);
+    let length = (800/a) - 2;
+    for (i = 0; i < totalSquares; i++) {
+        square.style.cssText += `width: ${length}px; height: ${length}px;`;
+        grid.appendChild(square.cloneNode(true));
+    }
 }
+
+makeGrid(69);
